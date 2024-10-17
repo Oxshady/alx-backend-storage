67a9ff70685f885c821a53a4f3281de49c794434
Oxshady <shadymahmod55@gmail.com>
2024-10-17 23:30:55 +0300

    Create SQL script to create 'users' table with required attributes

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -6,7 +6,6 @@
 -- If the table already exists, your script should not fail
 
 
-USE hbtn_0d_tvshows;
 CREATE TABLE IF NOT EXISTS users(id INT PRIMARY KEY AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255));
