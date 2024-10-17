5dfb447072bc404b6223b718deeb39c493a3b005
Oxshady <shadymahmod55@gmail.com>
2024-10-18 00:50:01 +0300

    Add SQL script to rank bands with Glam rock as their main style by longevity

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -3,8 +3,8 @@
 
 SELECT band_name,
 CASE
-	WHEN split IS NOT NULL THEN split - formed
-	ELSE 2021 - formed
+	WHEN split IS NOT NULL or split != 0 THEN split - formed
+	ELSE 2022 - formed
 END AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
