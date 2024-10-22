5354481bc44c29fe3ea66a9b39b4d7ef5c60e0ec
Oxshady <shadymahmod55@gmail.com>
2024-10-23 02:28:20 +0300

    styling

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-from pymongo import MongoClient
 """script that provides some stats about Nginx logs stored in MongoDB"""
+from pymongo import MongoClient
+
 
 if __name__ == "__main__":
     """main script"""
