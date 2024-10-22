740ff30f66cdc9982bc593d9b5ee0dc27c50c69f
Oxshady <shadymahmod55@gmail.com>
2024-10-23 00:33:08 +0300

    issue of type annoting

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
-from pymongo.collection import Collection
-from pymongo.cursor import Cursor
-from typing import Union, List, Dict
 """
 lists all documents in a collection
 """
 
 
-def list_all(mongo_collection: Collection) -> Union[Cursor[Dict], List]:
+def list_all(mongo_collection):
     """function that lists all documents in a collection"""
     documents = mongo_collection.find()
     if documents:
