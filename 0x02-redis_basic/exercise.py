13fab19619471712afb7eedb8b2612d1db18de70
Oxshady <shadymahmod55@gmail.com>
2024-10-23 19:13:07 +0300

    still failed at test cases

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,12 +16,12 @@ class Cache:
         Redis client and flush the database"""
 
         self._redis: Redis = Redis()
-        self._redis.flushdb(True)
+        self._redis.flushdb(asynchronous=True)
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """generate key then store data using
         that key and return the key"""
 
         uuid: str = str(uuid4())
-        self._redis.set(uuid, data)
+        self._redis.set(name=uuid, value=data)
         return uuid
