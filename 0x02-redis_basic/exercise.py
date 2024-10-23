8a8db7c0c4a280e0f189931ea4524a5a109dbe81
Oxshady <shadymahmod55@gmail.com>
2024-10-23 19:09:36 +0300

    fixing until checker accept the output

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -14,12 +14,14 @@ class Cache:
     def __init__(self) -> None:
         """Initialize the Cache instance with a
         Redis client and flush the database"""
+
         self._redis: Redis = Redis()
         self._redis.flushdb(asynchronous=True)
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """generate key then store data using
         that key and return the key"""
+
         uuid: str = str(uuid4())
         self._redis.set(name=uuid, value=data)
         return uuid
