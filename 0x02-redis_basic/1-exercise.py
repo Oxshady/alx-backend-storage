c77f809052404d83e10762b42bd3a873c7ef4ef0
Oxshady <shadymahmod55@gmail.com>
2024-10-23 18:47:48 +0300

    feat(cache): add Cache class with Redis client initialization and data storage
    - Initialize Redis client in the Cache class with flushdb to start with a clean database.
    - Implement  method to save data (str, bytes, int, float) in Redis with a randomly generated key.
    - Return the generated key after storing the data.
    
    This provides basic caching functionality using Redis with type annotations.

---
+++ 0x02-redis_basic/1-exercise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Initialize the Cache instance with a Redis client and flush the database."""
+from redis import Redis
+from uuid import uuid4
+from typing import Union
+
+
+class Cache:
+    """Cache class"""
+    def __init__(self) -> None:
+        """Initialize the Cache instance with a
+        Redis client and flush the database"""
+        self._redis = Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """generate key then store data using that key and return the key"""
+        uuid = uuid4().hex
+        self._redis.set(name=uuid, value=data)
+        return uuid
