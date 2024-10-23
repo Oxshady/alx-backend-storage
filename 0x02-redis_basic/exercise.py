2f5608bace5a683629615489c1d285f5572b3d70
Oxshady <shadymahmod55@gmail.com>
2024-10-23 19:17:53 +0300

    still try to make checker happy lol

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-"""Initialize the Cache instance with a Redis
-client and flush the database."""
+"""Initialize the Cache instance with
+a Redis client and flush the database."""
 
 
 from redis import Redis
@@ -15,13 +15,13 @@ class Cache:
         """Initialize the Cache instance with a
         Redis client and flush the database"""
 
-        self._redis: Redis = Redis()
-        self._redis.flushdb(asynchronous=True)
+        self._redis = Redis()
+        self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """generate key then store data using
-        that key and return the key"""
 
-        uuid: str = str(uuid4())
+        """generate key then store data
+        using that key and return the key"""
+        uuid = uuid4().hex
         self._redis.set(name=uuid, value=data)
         return uuid
